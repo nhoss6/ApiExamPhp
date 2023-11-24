b59a850c07ecd60e3391bf72ed32b0aa3be8ce60
Mohamed <mohamed.djabi@outlook.fr>
2023-11-24 21:27:03 +0100

    deuxieme exo OK

---
+++ src/Controller/SalaireController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SalaireController extends AbstractController
+{
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('/salaire', name: 'app_salaire')]
+    public function calculerSalaire(Request $request): Response
+    {
+        $salaireBrut = $request->query->get('salaireBrut');
+
+        $data = null;
+        if ($salaireBrut) {
+            $response = $this->client->request(
+                'POST',
+                'https://mon-entreprise.urssaf.fr/api/v1/evaluate',
+                [
+                    'json' => [
+                        'situation' => [
+                            'salarié . contrat . salaire brut' => [
+                                'valeur' => $salaireBrut,
+                                'unité' => '€ / mois'
+                            ],
+                            'salarié . contrat' => "'CDI'"
+                        ],
+                        'expressions' => [
+                            'salarié . rémunération . net . à payer avant impôt'
+                        ]
+                    ]
+                ]
+            );
+
+            if ($response->getStatusCode() === 200) {
+                $data = $response->toArray();
+            }
+        }
+
+        return $this->render('calcul_salaire.html.twig', [
+            'data' => $data,
+            'salaireBrut' => $salaireBrut
+        ]);
+    }
+}
